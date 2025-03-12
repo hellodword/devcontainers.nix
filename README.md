24401900e258e8baf9abcc9457f54432c21a833e
hellodword <46193371+hellodword@users.noreply.github.com>
2025-03-12 14:17:27 +0000

    feat: add layered image

--- README.md
+++ README.md
@@ -9,9 +9,16 @@
 - [x] github actions dynamic matrix
 - [x] github actions cache
   - [ ] audit
-- [ ] docker layers
+- [x] layered image
+- [ ] shell.nix to container
 - [ ] multiple arch
 
 ## Ref
 
 - https://github.com/niksi-aalto/niksi-devcontainer
+- https://github.com/divnix/std/blob/5b19a01095518d1acbd1975b1903b83ace4fe0dd/src/lib/ops/mkDevOCI.nix#L23
+  - https://github.com/divnix/std/blob/5b19a01095518d1acbd1975b1903b83ace4fe0dd/src/lib/ops/mkDevOCI.nix#L130
+  - https://github.com/search?q=%2F%28%3F-i%29%5B%5E%5C%2F%5DmkDevOCI%2F+language%3Anix&type=code
+- https://blog.eigenvalue.net/2023-nix2container-everything-once/
+- https://github.com/wi2trier/gpu-server/blob/e9b9c3723e6ed8ff8c7c332393b223a80095332d/overlays/image-base.nix
+- https://github.com/shikanime/shikanime/blob/ac87ec6cb43fe59c7b47eeb9b44dcfe66d2a8388/modules/nixos/hosts/devcontainer.nix
--- flake.nix
+++ flake.nix
@@ -127,6 +127,139 @@
                 chown -R ${username}:users /workspaces
               '';
             };
+
+          lib.mkLayeredDevcontainer =
+            {
+              pkgs,
+              name,
+              tag ? "latest",
+              packages ? [ ],
+              extensions ? [ ],
+              envVars ? { },
+            }:
+            let
+              lib = pkgs.lib;
+              username = "vscode";
+              extensionsJson = map (x: {
+                identifier.id = x.vscodeExtUniqueId;
+                version = x.version;
+                location = {
+                  "$mid" = 1;
+                  scheme = "file";
+                  path = "${x}/share/vscode/extensions/${x.vscodeExtUniqueId}";
+                };
+              }) extensions;
+              extensionsDirs = [
+                # VSCode
+                ".vscode-server"
+                # codespaces
+                ".vscode-remote"
+              ];
+              profile = pkgs.writeShellScript "profile" ''
+                if [ -d /etc/profile.d ]; then
+                  for i in /etc/profile.d/*.sh; do
+                    if [ -r $i ]; then
+                      . $i
+                    fi
+                  done
+                  unset i
+                fi
+              '';
+              extProfile = pkgs.writeShellScript "ext-profile" ''
+                if [ -f $HOME/.ext-profile ]; then
+                  exit 0
+                fi
+                touch $HOME/.ext-profile
+
+                ${builtins.concatStringsSep "\n" (
+                  map (extensionsDir: ''
+                    if [ ! -f $HOME/${extensionsDir}/extensions/extensions.json ]; then
+                      mkdir -p $HOME/${extensionsDir}/extensions
+                      echo '[]' > $HOME/${extensionsDir}/extensions/extensions.json
+
+                    fi
+                    cat > /tmp/extensions.json <<EOF
+                    ${builtins.toJSON extensionsJson}
+                    EOF
+                    ALL="$(jq -s '.[0] + .[1]' /tmp/extensions.json $HOME/${extensionsDir}/extensions/extensions.json)"
+                    echo "$ALL" > $HOME/${extensionsDir}/extensions/extensions.json
+                  '') extensionsDirs
+                )}
+
+                chown -R ${username}:${username} $HOME
+              '';
+              envVarsFull =
+                {
+                  # required by vscode terminal, but allow overriding
+                  SHELL = "/bin/bash";
+                }
+                // envVars
+                // {
+                  # required by vscode-server and its node
+                  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib${
+                    if builtins.hasAttr "LD_LIBRARY_PATH" envVars then ":${envVars.LD_LIBRARY_PATH}" else ""
+                  }";
+                };
+            in
+            pkgs.dockerTools.buildLayeredImage {
+              maxLayers = 125;
+              inherit name tag;
+              config = {
+                User = "${username}:${username}";
+                Env = map (x: "${x}=${envVarsFull."${x}"}") (builtins.attrNames envVarsFull);
+              };
+              contents = lib.lists.unique (
+                (with pkgs.dockerTools; [
+                  usrBinEnv
+                  binSh
+                  caCertificates
+                ])
+                ++ (with pkgs; [
+                  bash
+                  coreutils
+                  gnutar
+                  gzip
+                  gnused
+                  gnugrep
+
+                  jq
+                ])
+                ++ packages
+              );
+
+              extraCommands = ''
+                mkdir -m 1777 tmp
+
+                mkdir -p etc/profile.d
+                ln -s ${profile} etc/profile
+                ln -s ${extProfile} etc/profile.d/11-ext-profile.sh
+
+                # https://github.com/manesiotise/plutus-apps/blob/dbafa0ffdc1babcf8e9143ca5a7adde78d021a9a/nix/devcontainer-docker-image.nix#L99-L103
+                # allow ubuntu ELF binaries to run. VSCode copies it's own.
+                mkdir -p lib64
+                chmod +w lib64
+                ln -s ${pkgs.glibc}/lib64/ld-linux-x86-64.so.2 lib64/ld-linux-x86-64.so.2
+                ln -s ${pkgs.gcc-unwrapped.lib}/lib64/libstdc++.so.6 lib64/libstdc++.so.6
+                chmod -w lib64
+              '';
+
+              # https://github.com/devcontainers/features/blob/main/src/common-utils/main.sh
+              fakeRootCommands = ''
+                #!${pkgs.runtimeShell}
+                set -eux
+
+                ${pkgs.dockerTools.shadowSetup}
+
+                chmod 0777 tmp
+
+                groupadd --system --gid 1000 ${username}
+                useradd -s /bin/bash --system --uid 1000 --gid ${username} -m ${username}
+
+                mkdir -p /workspaces
+                chown ${username}:${username} /workspaces
+              '';
+              enableFakechroot = true;
+            };
         };
 
         perSystem =
@@ -226,6 +359,31 @@
                 }
                 // pkgs.lib.filterAttrs (n: _: n != "paths") overrides
               );
+
+              hello-layered = self.lib.mkLayeredDevcontainer (
+                {
+                  inherit pkgs;
+                  name = "hello-layered";
+                  packages =
+                    with pkgs;
+                    [
+                      git
+
+                      curl
+                    ]
+                    ++ overrides.paths or [ ];
+                  extensions =
+                    (with pkgs.vscode-extensions; [
+                      esbenp.prettier-vscode
+                    ])
+                    ++ (with pkgs.vscode-marketplace; [ ]);
+                  envVars = {
+                    FOO = "hello";
+                  };
+                }
+                // pkgs.lib.filterAttrs (n: _: n != "paths") overrides
+              );
+
             };
           };
       }
